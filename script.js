baa5e82c90be5182cbb84d8bdabadaf0531fdff6
Ankaleonova <ankaleonova@gmail.com>
2026-03-28 16:15:41 +0100

    newArr

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-function filterByInitial (list, character);
+function filterByInitial (list, character) {
+    const newArr = []
+    
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
